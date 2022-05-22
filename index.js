5e95f955b365410588d4e3afda325824fb0ada65
Alapottra <alapottratrojon@gmail.com>
2022-05-22 14:03:05 +0600

    added user collector and jwt requester

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config(); 
+require('dotenv').config();
 const port = process.env.PORT || 5000;
-const app = express(); 
+const app = express();
 
 
 //middlewares 
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -15,19 +15,41 @@ const uri = `mongodb+srv://ignition:${process.env.DB_PASS}@cluster0.kfnt4.mongod
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-const run = async() =>{
+const run = async () => {
 
 
-    try{
+    try {
 
         await client.connect();
         const database = client.db('ignition');
         const productCollection = database.collection('products');
+        const userCollection = database.collection('users');
+        const reviewCollection = database.collection('reviews');
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+
+        })
 
 
-        app.get('/products', async(req, res)=>{
-            const query = {}
+        app.get('/products', async (req, res) => {
+            const query = {};
             const result = await productCollection.find(query).toArray();
+            res.send(result);
+
+        })
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const result = await reviewCollection.find(query).toArray();
             res.send(result);
 
         })
@@ -36,7 +58,7 @@ const run = async() =>{
 
     }
 
-    finally{
+    finally {
 
 
 
@@ -57,6 +79,6 @@ run().catch(console.dir);
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('listening on port', port)
 })

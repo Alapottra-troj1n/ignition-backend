a8928ad8e010e9fb1b4fc8e0468817400ec60107
Alapottra <alapottratrojon@gmail.com>
2022-05-23 01:08:10 +0600

    added add order functionality and deduction from overall quantity functionality

--- index.js
+++ index.js
@@ -3,16 +3,36 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
-
+const jwt = require('jsonwebtoken');
 
 //middlewares 
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://ignition:${process.env.DB_PASS}@cluster0.kfnt4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//JWT Verify
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message : 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message : 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+
+
+    });
+}
+
+
 
 
 const run = async () => {
@@ -25,6 +45,7 @@ const run = async () => {
         const productCollection = database.collection('products');
         const userCollection = database.collection('users');
         const reviewCollection = database.collection('reviews');
+        const orderCollection = database.collection('orders');
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -35,7 +56,7 @@ const run = async () => {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            const token = jwt.sign({ email: email }, process.env.TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token });
 
         })
@@ -50,6 +71,32 @@ const run = async () => {
         app.get('/reviews', async (req, res) => {
             const query = {};
             const result = await reviewCollection.find(query).toArray();
+            res.send(result);
+
+        })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const results = await productCollection.findOne(query);
+            res.send(results);
+
+        })
+
+        //order 
+        app.post('/addorder', async (req, res) => {
+            const order = req.body;
+            const productName = order.product;
+            const filter = {name:productName}
+            const orderProduct = await productCollection.findOne(filter);
+            const updatedQuantity = orderProduct.availableQuantity - order.quantity;
+            const updatedDoc = {
+                $set: {
+                    availableQuantity: updatedQuantity
+                }
+            };
+            const updatedResult = await productCollection.updateOne(filter, updatedDoc);
+
+            const result = await orderCollection.insertOne(order);
             res.send(result);
 
         })
